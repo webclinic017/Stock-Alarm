98c93e1d528a333dc9bc67d22e12516007044c26
xlDownxl <niclas-joswig@gmx.de>
2019-11-08 00:32:31 +0900

    Backend pulls Alarms from Database now

--- index.js
+++ index.js
@@ -4,14 +4,13 @@ const port = 8080
 const socket = require('socket.io-client')('https://ws-api.iextrading.com/1.0/last')
 var admin = require('firebase-admin');
 
-var Alarms = []
 var symbols = []
 
 var serviceAccount = require("//home/niclas_joswig/Stock-Alarm/stock-alarm-27fd9-firebase-adminsdk-ckcdi-05ef23eac1.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://stock-alarm-27fd9.firebaseio.com"
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://stock-alarm-27fd9.firebaseio.com"
 });
 
 var registrationToken = 'eVuajnwLCwQ:APA91bGZhbXnGnsP5F5TwwB07AoLnpOGPPhSE02LvglYE5m1MJ_pFYQuunBvJD8xVKwpm_wm1-QuP0mm4N6JCSpUWHzWGmvIyGtU2tQZWgbd88kwjNn4j6dJ6goWs-6NdBLUif-1rhky';
@@ -22,7 +21,7 @@ class Alarm {
         this.symbol = symbol;
         this.level = level;
         this.owner = owner;  //needed for push notification
-        this.alarm_id = alarmId;
+        this.id = alarmId;
     }
     toString() {
         return "  Symbol: " + this.symbol + ", Price of Alarm: " + this.level;
@@ -30,53 +29,68 @@ class Alarm {
 }
 
 //dummy data
-Alarms = [new Alarm('AAPL', 260.0, "John", "id1"), new Alarm('GOOGL', 150, "Sarah","id2")]
+//Alarms = [new Alarm('AAPL', 260.0, "John", "id1"), new Alarm('GOOGL', 150, "Sarah","id2")]
 symbols = ['AAPL,GOOGL']
-last_prices = { 'AAPL': 140, 'GOOGL': 210 }
+last_prices = { 'AAPL': 259, 'GOOGL': 210 }
 
-function notifyUser(symbol,owner,alarmId_)
-{
+function notifyUser(symbol, owner, alarmId_) {
     var message = {
-        notification: {title: symbol},
-  data: {
-    alarmId: alarmId_,
-  },
-  token: registrationToken
-};
-
+        notification: { title: symbol },
+        data: {
+            alarmId: alarmId_,
+        },
+        token: registrationToken
+    };
     admin.messaging().send(message)
-  .then((response) => {
-    // Response is a message ID string.
-    console.log('Successfully sent message:', response);
-  })
-  .catch((error) => {
-    console.log('Error sending message:', error);
-  });
+        .then((response) => {
+            // Response is a message ID string.
+            console.log('Successfully sent message:', response);
+        })
+        .catch((error) => {
+            console.log('Error sending message:', error);
+        });
 
 }
 
 function checkAlarms(data) {
+    Alarms = []
     updateSymbol = data['symbol']
-    for (i = 0; i < Alarms.length; i++) {
-        if (Alarms[i].symbol == updateSymbol) {
+    price = data['price']
+    console.log("price start:     " + price)
+    console.log("last price start:   " + last_prices[updateSymbol])
+    var db = admin.database();
+    var ref = db.ref("Alarms").child(updateSymbol).once("value", (snapshot) => {
 
+        //here catch all promises and return them to outer promise (promise.all)
+        snapshot.forEach(function(childSnapshot) {
+            //console.log(childSnapshot.val())
+            var alarm = childSnapshot.val()
+            Alarms.push(new Alarm(alarm.symbol, alarm.level, alarm.owner, alarm.id))
+        })
+    }).then((ds) => {
+        //console.log(Alarms)
+        for (i = 0; i < Alarms.length; i++) {
             if (Alarms[i].level < last_prices[updateSymbol]) {
-                if (Alarms[i].level <= data['price']) {
+                if (Alarms[i].level >= price) {
                     console.log("ALARM")
-                    notifyUser(updateSymbol,Alarms[i].owner,Alarms[i].alarmId)
-                    Alarms.splice(i,1)
+                    console.log("price:  " + price)
+                    console.log("last price:   " + last_prices[updateSymbol])
+                    console.log("Alarm level:    " + Alarms[i].level)
+                    //notifyUser(updateSymbol,Alarms[i].owner,Alarms[i].id)
+                    Alarms.splice(i, 1)
                 }
             }
             else {
-                if (Alarms[i].level >= data['price']) {
+                if (Alarms[i].level <= data['price']) {
                     console.log("ALARM")
-                    notifyUser(updateSymbol,Alarms[i].owner,Alarms[i].alarmId)
-                    Alarms.splice(i,1)
+                    //notifyUser(updateSymbol,Alarms[i].owner,Alarms[i].id)
+                    Alarms.splice(i, 1)
                 }
             }
-            last_prices[updateSymbol] == data['price']
+            
         }
-    }
+        last_prices[updateSymbol] = price
+    }).catch((reason) => console.log("failure"))
 }
 
 //todo: http get function for receiving alarm from the user
