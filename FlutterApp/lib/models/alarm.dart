d036a57abc8fa0df9096acd69c4eb4b1df93c45f
Niclas <niclas.joswig@helsinki.fi>
2019-11-16 23:03:42 +0900

    Added trigger Date App functionality

--- FlutterApp/lib/models/alarm.dart
+++ FlutterApp/lib/models/alarm.dart
@@ -5,6 +5,7 @@ class Alarm {
   double level;
   String id;
   DateTime creationDate;
+  DateTime triggerDate;
 
   Alarm(symbol, level) {
     this.symbol = symbol;
@@ -16,12 +17,17 @@ class Alarm {
     this.id = id;
   }
 
-  Alarm.fromMap(Map snapshot)
-      : id = snapshot['id'] ?? '',
-        symbol = snapshot['symbol'] ?? '',
-        level = snapshot['level'].toDouble() ?? '',
-        creationDate =
-            DateTime.fromMillisecondsSinceEpoch(snapshot['creationDate']) ?? '';
+  Alarm.fromMap(Map snapshot) {
+    id = snapshot['id'] ?? '';
+    symbol = snapshot['symbol'] ?? '';
+    level = snapshot['level'].toDouble() ?? '';
+    creationDate =
+        DateTime.fromMillisecondsSinceEpoch(snapshot['creationDate']) ?? '';
+    if (snapshot["triggerDate"] != null) {
+      triggerDate = DateTime.fromMillisecondsSinceEpoch(snapshot[
+          "triggerDate"]); //TODO send triggerdate with push message and display it in log screen
+    }
+  }
 
   String toString() {
     return "Alarm of Symbol $symbol at Level ${level.toString()} created at ${DateFormat("dd-MM-yy").add_j().format(creationDate)}";
